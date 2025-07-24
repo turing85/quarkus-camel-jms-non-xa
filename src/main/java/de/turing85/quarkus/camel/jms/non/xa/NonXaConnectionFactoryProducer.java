5eaaa5de16d4734e50728897fca19fb77fa2b5d2
Marco Bungart <marco.bungart@googlemail.com>
2025-07-24 19:44:03 +0200

    Add pooling

--- src/main/java/de/turing85/quarkus/camel/jms/non/xa/NonXaConnectionFactoryProducer.java
+++ src/main/java/de/turing85/quarkus/camel/jms/non/xa/NonXaConnectionFactoryProducer.java
@@ -5,21 +5,34 @@ import java.util.Objects;
 import jakarta.enterprise.inject.Produces;
 import jakarta.jms.ConnectionFactory;
 
+import io.quarkiverse.messaginghub.pooled.jms.PooledJmsRuntimeConfig;
+import io.quarkiverse.messaginghub.pooled.jms.PooledJmsWrapper;
 import io.quarkus.artemis.core.runtime.ArtemisRuntimeConfig;
 import io.quarkus.artemis.core.runtime.ArtemisRuntimeConfigs;
 import io.quarkus.artemis.core.runtime.ArtemisUtil;
 import io.smallrye.common.annotation.Identifier;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.messaginghub.pooled.jms.JmsPoolConnectionFactory;
 
 public class NonXaConnectionFactoryProducer {
   public static final String IDENTIFIER = ArtemisUtil.DEFAULT_CONFIG_NAME + "-non-xa";
 
   @Produces
   @Identifier(IDENTIFIER)
-  ConnectionFactory nonXaConnectionFactory(ArtemisRuntimeConfigs runtimeConfigs) {
-    final ArtemisRuntimeConfig defaultConfig =
-        Objects.requireNonNull(runtimeConfigs.configs().get(ArtemisUtil.DEFAULT_CONFIG_NAME));
-    return new ActiveMQConnectionFactory(defaultConfig.getUrl(), defaultConfig.getUsername(),
-        defaultConfig.getPassword());
+  ConnectionFactory nonXaConnectionFactory(final ArtemisRuntimeConfigs runtimeConfigs,
+      final PooledJmsRuntimeConfig poolConfig) {
+    return constructPooledNonXaConnectionFactory(
+        Objects.requireNonNull(runtimeConfigs.configs().get(ArtemisUtil.DEFAULT_CONFIG_NAME)),
+        poolConfig);
   }
+
+  private static JmsPoolConnectionFactory constructPooledNonXaConnectionFactory(
+      ArtemisRuntimeConfig config, PooledJmsRuntimeConfig poolConfig) {
+    final JmsPoolConnectionFactory poolConnectionFactory = new JmsPoolConnectionFactory();
+    PooledJmsWrapper.pooledJmsRuntimeConfigureConnectionFactory(poolConnectionFactory,
+        new ActiveMQConnectionFactory(config.getUrl(), config.getUsername(), config.getPassword()),
+        poolConfig);
+    return poolConnectionFactory;
+  }
+
 }
